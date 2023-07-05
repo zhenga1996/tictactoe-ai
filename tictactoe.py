fe0a1f3ee07c8d34e9417caf2fd28f3847467e35
Allen Zheng <volcaronitee8533@gmail.com>
2023-07-05 14:12:22 -0400

    Adjusted organization of code

--- tictactoe.py
+++ tictactoe.py
@@ -1,6 +1,5 @@
 import pygame
 from enum import Enum
-from util import is_winner, is_tie
 
 pygame.init()
 font = pygame.font.Font('arial.ttf', 75)
@@ -40,6 +39,41 @@ class TTTGame:
         self.board = [[None for i in range(self.board_size)] for j in range(self.board_size)]
         self.draw_ui()
 
+    def is_winner(self):
+        for i in range(2, len(self.board)):
+            for j in range(len(self.board)):
+                # Detect Horizontal
+                if self.board[i][j] == self.turn and \
+                        self.board[i - 1][j] == self.turn and \
+                        self.board[i - 2][j] == self.turn:
+                    return True
+                # Detect Vertical
+                if self.board[j][i] == self.turn and \
+                        self.board[j][i - 1] == self.turn and \
+                        self.board[j][i - 2] == self.turn:
+                    return True
+                # Detect Diagonals
+                # \
+                if j >= 2 and self.board[i][j] == self.turn and \
+                        self.board[i - 1][j - 1] == self.turn and \
+                        self.board[i - 2][j - 2] == self.turn:
+                    return True
+                # /
+                if j < len(self.board) - 2 and \
+                        self.board[i][j] == self.turn and \
+                        self.board[i - 1][j + 1] == self.turn and \
+                        self.board[i - 2][j + 2] == self.turn:
+                    return True
+
+        return False
+
+    def is_tie(self):
+        for i in range(len(self.board)):
+            if None in self.board[i]:
+                return False
+
+        return True
+
     def play_step(self):
         # 1: collect user input
         result = None
@@ -58,37 +92,36 @@ class TTTGame:
                     break
 
                 # 2: Check for winner or tie
-                if is_winner(self.turn, self.board):
+                if self.is_winner():
                     result = self.turn
-                if is_tie(self.board):
+                if self.is_tie():
                     result = 2
 
                 self.turn = not self.turn
 
                 # 3: Draw Board
                 self.draw_ui()
-
         return result
 
     def draw_ui(self):
         self.display.fill(BLACK)
         for i in range(0, self.board_size):
             for j in range(0, self.board_size):
-                rect = pygame.Rect(i*BLOCK_SIZE, j*BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE)
+                rect = pygame.Rect(i * BLOCK_SIZE, j * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE)
                 pygame.draw.rect(self.display, WHITE, rect, 1)
 
                 if self.board[i][j] is False:
-                    self.display.blit(font.render('O', False, BLUE), ((i+0.1)*BLOCK_SIZE, j*BLOCK_SIZE))
+                    self.display.blit(font.render('O', False, BLUE), ((i + 0.1) * BLOCK_SIZE, j * BLOCK_SIZE))
                 elif self.board[i][j] is True:
-                    self.display.blit(font.render('X', False, RED), ((i+0.1)*BLOCK_SIZE, j*BLOCK_SIZE))
+                    self.display.blit(font.render('X', False, RED), ((i + 0.1) * BLOCK_SIZE, j * BLOCK_SIZE))
 
         pygame.display.flip()
 
 
 if __name__ == '__main__':
-    # Create game
+    # create game
     while True:
-        num = input('Enter game speed [1-10]: ')
+        num = input('Enter game size [1-10]: ')
         if num.isdigit() and 0 <= int(num) <= 10:
             break
         else:
